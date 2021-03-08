5ebac32ec17d25aa38e48ee6e1cef677a977d045
Asep Budiyana M <asepbudiyana12@gmail.com>
2021-03-08 17:47:12 +0700

    assignment 1

---
+++ 140810180029_Tugas1/pertemuan1pacd/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace pertemuan1pacd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 37);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 197);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(815, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openImageToolStripMenuItem
+            // 
+            this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            this.openImageToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openImageToolStripMenuItem.Text = "Open Image";
+            this.openImageToolStripMenuItem.Click += new System.EventHandler(this.openImageToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(278, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(371, 37);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(278, 72);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(371, 72);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(463, 72);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(76, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(463, 37);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 19);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Get RGB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(415, 109);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 20);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Brightness";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(563, 37);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(237, 197);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(322, 109);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(76, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Invert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(279, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Grayscale (avg)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(431, 181);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 22);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Grayscale (weight)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(415, 221);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Thresholding";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(322, 224);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(76, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 256);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Tugas1 - 140810180029";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ 140810180029_Tugas1/pertemuan1pacd/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pertemuan1pacd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.bmp) | *.jpg; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK) 
+            {
+                Bitmap gambar = new Bitmap(open.FileName);
+                pictureBox1.Image = new Bitmap(gambar, new Size(200,200));
+            }
+        }
+
+        Bitmap bmp;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Menentukan pixel yang mau dilihat RGB-nya
+            int x = Convert.ToInt16(textBox1.Text);
+            int y = Convert.ToInt16(textBox2.Text);
+
+            // memuat gambar dari picture box
+            bmp = (Bitmap)pictureBox1.Image;
+
+            //Mengambil nilai warna dari pixel yang ditentukan
+            int r = bmp.GetPixel(x, y).R;
+            int g = bmp.GetPixel(x, y).G;
+            int b = bmp.GetPixel(x, y).B;
+
+            //Menampilkan warna tiap pixel pada textbox
+            textBox3.Text = r.ToString();
+            textBox4.Text = g.ToString();
+            textBox5.Text = b.ToString();
+
+        }
+
+        Bitmap bmpAsli, bmpHasil;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt16(textBox6.Text);
+            int i, j;
+            int nilaiR, nilaiG, nilaiB;
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+            Cursor = Cursors.WaitCursor;
+            for (i = 0; i < baris; i++) 
+            {
+                for(j = 0; j<kolom; j++) 
+                {
+                    nilaiR = bmpAsli.GetPixel(i,j).R + k;
+                    nilaiG = bmpAsli.GetPixel(i, j).G + k;
+                    nilaiB = bmpAsli.GetPixel(i, j).B + k;
+                    if (nilaiR > 255) nilaiR = 255;
+                    if (nilaiG > 255) nilaiG = 255;
+                    if (nilaiB > 255) nilaiB = 255;
+                    if (nilaiR < 0) nilaiR = 0;
+                    if (nilaiG < 0) nilaiG = 0;
+                    if (nilaiB < 0) nilaiB = 0;
+                    bmpHasil.SetPixel(i, j, Color.FromArgb(nilaiR, nilaiG, nilaiB));
+                }
+            }
+            pictureBox2.Image = bmpHasil;
+            Cursor = Cursors.Default;
+        }
+ 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            int nilaiR, nilaiG, nilaiB;
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+            Cursor = Cursors.WaitCursor;
+            for (i = 0; i < baris; i++)
+            {
+                for (j = 0; j < kolom; j++)
+                {
+                    nilaiR = 255 - bmpAsli.GetPixel(i, j).R;
+                    nilaiG = 255 - bmpAsli.GetPixel(i, j).G;
+                    nilaiB = 255 - bmpAsli.GetPixel(i, j).B;
+                    bmpHasil.SetPixel(i, j, Color.FromArgb(nilaiR, nilaiG, nilaiB));
+                }
+            }
+            pictureBox2.Image = bmpHasil;
+            Cursor = Cursors.Default;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            int nilaiR, nilaiG, nilaiB, grayscale;
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+            Cursor = Cursors.WaitCursor;
+            for (i = 0; i < baris; i++)
+            {
+                for (j = 0; j < kolom; j++)
+                {
+                    nilaiR = bmpAsli.GetPixel(i, j).R;
+                    nilaiG = bmpAsli.GetPixel(i, j).G;
+                    nilaiB = bmpAsli.GetPixel(i, j).B;
+                    grayscale = (nilaiR + nilaiG + nilaiB)/3;
+                    bmpHasil.SetPixel(i, j, Color.FromArgb(grayscale, grayscale, grayscale));
+                }
+            }
+            pictureBox2.Image = bmpHasil;
+            Cursor = Cursors.Default;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            int nilaiR, nilaiG, nilaiB, grayscale;
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+            Cursor = Cursors.WaitCursor;
+            for (i = 0; i < baris; i++)
+            {
+                for (j = 0; j < kolom; j++)
+                {
+                    nilaiR = (int) (bmpAsli.GetPixel(i, j).R * .299);
+                    nilaiG = (int) (bmpAsli.GetPixel(i, j).G * .587);
+                    nilaiB = (int) (bmpAsli.GetPixel(i, j).B * .114);
+                    grayscale = nilaiR + nilaiG + nilaiB;
+                    bmpHasil.SetPixel(i, j, Color.FromArgb(grayscale, grayscale, grayscale));
+                }
+            }
+            pictureBox2.Image = bmpHasil;
+            Cursor = Cursors.Default;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int t = Convert.ToInt16(textBox7.Text);
+            if (t > 255) t = 255;
+            if (t < 0) t = 0;
+
+            int i, j;
+            int nilaiR, nilaiG, nilaiB;
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+            Cursor = Cursors.WaitCursor;
+            for (i = 0; i < baris; i++)
+            {
+                for (j = 0; j < kolom; j++)
+                {
+                    nilaiR = bmpAsli.GetPixel(i, j).R;
+                    nilaiG = bmpAsli.GetPixel(i, j).G;
+                    nilaiB = bmpAsli.GetPixel(i, j).B;
+
+                    nilaiR = nilaiR > t ? 255 : 0;
+                    nilaiG = nilaiG > t ? 255 : 0;
+                    nilaiB = nilaiB > t ? 255 : 0;
+                    bmpHasil.SetPixel(i, j, Color.FromArgb(nilaiR, nilaiG, nilaiB));
+                }
+            }
+            pictureBox2.Image = bmpHasil;
+            Cursor = Cursors.Default;
+        }
+    }
+}
