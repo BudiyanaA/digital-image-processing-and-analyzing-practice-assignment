b2c675b5c2718a74612cf2171578463667c9dedc
Asep Budiyana M <asepbudiyana12@gmail.com>
2021-03-22 18:16:23 +0700

    pointer based

---
+++ 140810180029_Tugas2/pertemuan2pacd/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace pertemuan2pacd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 189);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(482, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(252, 189);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(749, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem,
+            this.saveImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openImageToolStripMenuItem
+            // 
+            this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            this.openImageToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openImageToolStripMenuItem.Text = "Open Image";
+            this.openImageToolStripMenuItem.Click += new System.EventHandler(this.openImageToolStripMenuItem_Click);
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.saveImageToolStripMenuItem.Text = "Save Image";
+            this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Grayscale";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(349, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Brightness";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(349, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Invert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(284, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(390, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Thresholding";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Operation Time :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0 ms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 273);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Tugas2 - 140810180029";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 140810180029_Tugas2/pertemuan2pacd/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pertemuan2pacd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            saveImageToolStripMenuItem.Enabled = false;
+        }
+
+        private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.bmp) | *.jpg; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap gambar = new Bitmap(open.FileName);
+                pictureBox1.Image = new Bitmap(gambar, new Size(200, 200));
+            }
+        }
+
+        private void saveImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool isSuccess = false;
+            string resultMessage = "File save {0}. {1}";
+
+            try
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Image Files(*.jpg; *.bmp) | *.jpg; *.bmp";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap drawImage = (Bitmap)pictureBox2.Image;
+                    drawImage.Save(dialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            
+                isSuccess = true;
+                resultMessage = string.Format(resultMessage, "succeeded", string.Empty);
+            }
+            catch (Exception errorSave)
+            {
+                resultMessage = string.Format(resultMessage, "failed", errorSave.Message);
+            }
+            finally
+            {
+                MessageBox.Show(resultMessage, isSuccess ? "Success" : "Error");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        Bitmap bmpAsli, bmpHasil;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+
+            Cursor = Cursors.WaitCursor;
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            System.Drawing.Imaging.BitmapData bmData = bmpAsli.LockBits(new Rectangle(0, 0, bmpAsli.Width, bmpAsli.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            unsafe
+            {
+                byte* p = (byte*)(void*)bmData.Scan0.ToPointer();
+                int stopAddress = (int)p + bmData.Stride * bmData.Height;
+                while ((int)p != stopAddress)
+                {
+                    p[0] = (byte)(.299 * p[2] + .587 * p[1] + .114 * p[0]);
+                    p[1] = p[0];
+                    p[2] = p[0];
+                    p += 3;
+                }
+            }
+            bmpAsli.UnlockBits(bmData);
+            pictureBox2.Image = bmpAsli;
+            saveImageToolStripMenuItem.Enabled = true;
+            Cursor = Cursors.Default;
+            sw.Stop();
+            label2.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+
+            Cursor = Cursors.WaitCursor;
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            System.Drawing.Imaging.BitmapData data = bmpAsli.LockBits(new Rectangle(0, 0, bmpAsli.Width, bmpAsli.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            int nOffset = data.Stride - data.Width * 3, nVal, nBrightness = 50;
+            int nWidth = data.Width * 3;
+            unsafe
+            {
+                byte* ptr = (byte*)(data.Scan0);
+                for (int y = 0; y < data.Height; ++y)
+                {
+                    for (int x = 0; x < nWidth; ++x)
+                    {
+                        nVal = (int)(ptr[0] + nBrightness);
+                        if (nVal < 0) nVal = 0;
+                        if (nVal > 255) nVal = 255;
+                        ptr[0] = (byte)nVal;
+                        ++ptr;
+                    }
+                    ptr += nOffset;
+                }
+            }
+
+            bmpAsli.UnlockBits(data);
+            pictureBox2.Image = bmpAsli;
+            saveImageToolStripMenuItem.Enabled = true;
+            Cursor = Cursors.Default;
+            sw.Stop();
+            label2.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bmpAsli = (Bitmap)pictureBox1.Image;
+            int baris = bmpAsli.Width;
+            int kolom = bmpAsli.Height;
+            bmpHasil = new Bitmap(baris, kolom);
+
+            Cursor = Cursors.WaitCursor;
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            System.Drawing.Imaging.BitmapData bmData = bmpAsli.LockBits(new Rectangle(0, 0, bmpAsli.Width, bmpAsli.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            unsafe
+            {
+                byte* p = (byte*)(void*)bmData.Scan0.ToPointer();
+                int stopAddress = (int)p + bmData.Stride * bmData.Height;
+                while ((int)p != stopAddress)
+                {
+                    p[0] = (byte)(255 - p[0]);
+                    p[1] = (byte)(255 - p[1]);
+                    p[2] = (byte)(255 - p[2]);
+                    p += 3;
+                }
+            }
+            bmpAsli.UnlockBits(bmData);
+            pictureBox2.Image = bmpAsli;
+            saveImageToolStripMenuItem.Enabled = true;
+            Cursor = Cursors.Default;
+            sw.Stop();
+            label2.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int t = Convert.ToInt16(textBox1.Text);
+            if (t > 255)
+            {
+                MessageBox.Show("Thresholding value > 255", "Error");
+            }
+            else if (t < 0)
+            {
+                MessageBox.Show("Thresholding value < 0", "Error");
+            }
+            else
+            {
+                bmpAsli = (Bitmap)pictureBox1.Image;
+                int baris = bmpAsli.Width;
+                int kolom = bmpAsli.Height;
+                bmpHasil = new Bitmap(baris, kolom);
+
+                Cursor = Cursors.WaitCursor;
+                System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+                System.Drawing.Imaging.BitmapData bmData = bmpAsli.LockBits(new Rectangle(0, 0, bmpAsli.Width, bmpAsli.Height),
+                    System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                unsafe
+                {
+                    byte* p = (byte*)(void*)bmData.Scan0.ToPointer();
+                    int stopAddress = (int)p + bmData.Stride * bmData.Height;
+                    while ((int)p != stopAddress)
+                    {
+                        p[0] = (byte)(p[0] > t ? 255 : 0);
+                        p[1] = (byte)(p[1] > t ? 255 : 0);
+                        p[2] = (byte)(p[2] > t ? 255 : 0);
+                        p += 3;
+                    }
+                }
+                bmpAsli.UnlockBits(bmData);
+                pictureBox2.Image = bmpAsli;
+                saveImageToolStripMenuItem.Enabled = true;
+                Cursor = Cursors.Default;
+                sw.Stop();
+                label2.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";
+            }
+        }
+
+    }
+}
